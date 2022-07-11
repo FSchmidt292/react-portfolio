78a0688efa7ecbc7fc073cd17935ae48d0ec0f69
Trey S <fjschmidt292@gmail.com>
2022-07-11 19:49:59 -0400

    restructuring

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import About from './components/about/About.jsx'
+import Header from './components/header/Header.jsx'
+import Nav from './components/nav/Nav.jsx'
+import Portfolio from './components/portfolio/Portfolio.jsx'
+import Contact from './components/contact/Contact.jsx'
+import Footer from './components/footer/Footer.jsx'
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Portfolio />
+        <Contact />
+        <Footer />
+    </>
+  )
+}
+
+export default App
