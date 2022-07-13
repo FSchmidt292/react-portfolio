78bedd688a0636a5c5c8892e3d271017b57a2ad5
Trey S <fjschmidt292@gmail.com>
2022-07-13 06:44:00 -0400

    contact and about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './about.css'
+import Scuba from '../../assets/scuba.png'
 
 const About = () => {
   return (
-    <div id='about'> About</div>
+    <section id='about' className='section1'>
+      <h1>About Me:</h1>
+      <div className="container">
+        
+          <div className="aboutme_img">
+            <img src={Scuba} className="aboutImg" alt="me getting my Scuba Certification in an olympic pool"/>
+          </div>
+          <div className="aboutme">
+            <div className="aboutMeCard bio">
+              <h3>Bio</h3>
+              <p>I'm a fresh bootcamp graduate coming from a Case Western Reserve University Full Stack Development bootcamp. I'm eager to get started!</p>
+            </div>
+            <div className="aboutMeCard experience">
+              <h3>Technologies</h3>
+              <p>I have have experience with HTML, CSS, Javascript, MySQL, mongoose, react, node.js, and express.js</p>
+            </div>
+            <div className="aboutMeCard resume">
+              <h3>Resume</h3>
+              <p>Download my resume here!</p>
+            </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,19 +1,42 @@
 import React from 'react'
 import './contact.css'
-
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_krvnkgm', 'template_5ygfm2a', form.current, 'm0ArX1RGtYjCDbEFq')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) =>{
+        console.log(error.text);
+      });
+  };
+
   return (
-    <div id='contact'>
-      <div className="container contact__container">
-        <h1> Contact me!</h1>
-          <form action="mailto:fjschmidt292@gmail.com">
-            <input type="name"></input>
-            <input type="email"></input>
-            <input type="message"></input>
-          </form>
+    <section id='contact' className='section1'>
+      <h1> Contact me!</h1>
+      <p>Send me a message through your email client by clicking the link OR type your message here in the browser</p>
+        <div className="container contact__container">
+          
+            <div className="email">
+              <h3>FJschmidt292@gmail.com</h3>
+              <a href="mailto:FJschmidt292@gmail.com">Send a Message</a>
+            </div>
+            <div className="submissionForm">
+            <form ref={form} onSubmit={sendEmail}>
+              <input type='text' name='name' placeholder='Your Name Here!' required />
+              <input type='email' name='email' placeholder='Your eMail Here!' required />
+              <textarea name='message' placeholder='Your Message Here!' required></textarea>
+              <button type='submit' className='btn'>Send Message</button>
+            </form>
+          </div>
         </div>
-    </div>
+      </section>
   )
 }
 
