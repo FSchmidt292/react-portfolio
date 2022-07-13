5014189a152afc9b905b0ad0458a7ab445750df8
Trey S <fjschmidt292@gmail.com>
2022-07-13 08:24:28 -0400

    templating portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,7 +7,7 @@ function Portfolio() {
     <section id='portfolio'>
       <h1>Portfolio</h1>
       <h3>Some of my key projects</h3>
-        <div className='container'>
+        <div className='projectContainer'>
           <div className='project'>
             <Project />
           </div>
